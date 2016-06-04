ae437f4a6c767a2804e63468e2560a98424a6356
Clécio Varjão <cleciovarjao@gmail.com>
2016-06-03 21:59:25 -0700

    Update run.sh

--- files/app/phpmyadmin/run.sh
+++ files/app/phpmyadmin/run.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 set -x
+
+if [ ! -f /app/phpmyadmin/www/config.secret.inc.php ] ; then
+    cat > /app/phpmyadmin/www/config.secret.inc.php <<EOT
+<?php
+\$cfg['blowfish_secret'] = '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`';
+EOT
+fi
+    
+exec php -S 0.0.0.0:80 -t /app/phpmyadmin/www/ \
+    -d upload_max_filesize=$PHP_UPLOAD_MAX_FILESIZE \
+    -d post_max_size=$PHP_UPLOAD_MAX_FILESIZE \
+    -d max_input_vars=$PHP_MAX_INPUT_VARS
