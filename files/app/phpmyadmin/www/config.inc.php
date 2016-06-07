ce9c9334e7c46de83cba7c86626d31644cdf5fd3
Clécio Varjão <cleciovarjao@gmail.com>
2016-06-06 18:32:13 -0700

    Update config.inc.php

--- files/app/phpmyadmin/www/config.inc.php
+++ files/app/phpmyadmin/www/config.inc.php
@@ -32,7 +32,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] =  '172.16.121.149';
+$cfg['Servers'][$i]['host'] =  $_ENV['MYSQL_SERVICE_HOST'];
 /*$_ENV['MYSQL_SERVICE_HOST']; */
 /*MYSQL_SERVICE_HOST */
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
